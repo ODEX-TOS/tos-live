d71d5d6bdc5de43b318e7f86052e3e47b10c8723
F0xedb <tommeyers.tm127@gmail.com>
2019-07-17 11:00:25 +0000

    Upload tos live if an image has been build

--- toslive/start.sh
+++ toslive/start.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# This is a simple script to build an iso image. It prepares the build directory and then starts the build
+
+# Install needed dependencies
+if [[ "$(which mkarchiso)" != "/usr/bin/mkarchiso" ]]; then
+    yay -Syu archiso
+fi
+
 rm -v work/build.make_*
 
 ./build.sh -v
+
+if [[ ! -d "images" ]]; then
+        mkdir images
+fi
+
+cp out/*.iso images/
+
+mv out/*.iso out/toslive.iso
