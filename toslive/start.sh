c301e796d4b352e1df362b3e01e3564bf395afa6
F0xedb <tommeyers.tm127@gmail.com>
2019-07-18 17:12:48 +0200

    Added tos server iso build possibility

--- toslive/start.sh
+++ toslive/start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [[ "$1" == "-g" ]]; then
+    sed -i 's;gui="0";gui="1";' airootfs/root/customize_airootfs.sh
+    cp packages.x86_64_client packages.X86_64
+fi
+
+if [[ "$1" == "-s" ]]; then
+    sed -i 's;gui="1";gui="O";' airootfs/root/customize_airootfs.sh
+    cp packages.x86_64_server packages.X86_64
+fi
+
+
+if [[ "$1" == "-h" ]]; then
+        echo "-h | help message"
+        echo "-s | compile iso in server mode"
+        echo "-g | compile iso in gui mode "
+fi
+
 rm -v work/build.make_*
 
 ./build.sh -v
