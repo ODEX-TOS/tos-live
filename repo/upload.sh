0f1d4836962cbf752bd98f3765de75d4365a9c49
Tom <tommeyers.tm127@gmail.com>
2019-08-06 15:57:16 +0000

    Update upload.sh

--- repo/upload.sh
+++ repo/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script will take the content of arch/* and upload it to the TOS server.
-
-read -p "Only upload if this is a full repo otherwise the repo database is wrong"
+if [[ "$1" != "-y" ]]; then
+  read -p "Only upload if this is a full repo otherwise the repo database is wrong"
+fi
 rsync -rl arch root@pbfp.xyz:/root/repo/
