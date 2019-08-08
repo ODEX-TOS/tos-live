362864c5fdae3a7e089871eb78f6f59b1a03e03a
Tom <tommeyers.tm127@gmail.com>
2019-08-08 23:24:49 +0200

    Update version_builder.sh

--- toslive/version_builder.sh
+++ toslive/version_builder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Build the current version based on the amount of commits. It makes sure the version is always unique
 commit=$(git rev-list --count HEAD)
 version=$(cat version-edit.txt)
 echo "$version"-"$commit" > version.txt
