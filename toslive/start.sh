4bd7a8bb08cc4e94d8f7cf92fd91e86f6ce97e3e
F0xedb <tom@odex.be>
2019-09-12 12:57:23 +0200

    Added a kde plasma option as a default desktop.
    We need to customize the kde version however to suit our needs. Eg
    change the default theme, set st as default instead of konsole etc.

--- toslive/start.sh
+++ toslive/start.sh
@@ -27,6 +27,9 @@ function build {
     if [[ ! -d "images/client" ]]; then
         mkdir -p images/client
     fi
+    if [[ ! -d "images/kde" ]]; then
+        mkdir -p images/kde
+    fi
 
 
 
@@ -37,6 +40,16 @@ function build {
         else
             cp out/toslinux*.iso images/client/"$iso_normal""$append".iso
             mv out/toslinux*.iso out/toslive.iso
+        fi
+    fi
+
+    if [[ "$1" == "-k" ]]; then
+        if [[ "$2" == "-a" ]]; then
+            cp out/toslinux*.iso images/kde/"$iso_azerty""$append".iso
+            mv out/toslinux*.iso out/toslive-kde-azerty.iso
+        else
+            cp out/toslinux*.iso images/kde/"$iso_normal""$append".iso
+            mv out/toslinux*.iso out/toslive-kde.iso
         fi
     fi
 
@@ -56,6 +69,8 @@ if [[ "$1" == "-g" ]]; then
         cp customize_airootfs.sh_client airootfs/root/customize_airootfs.sh || exit 1
 elif [[ "$1" == "-s" ]]; then
         cp customize_airootfs.sh_server airootfs/root/customize_airootfs.sh || exit 1
+elif [[ "$1" == "-k" ]]; then
+        cp customize_airootfs.sh_kde airootfs/root/customize_airootfs.sh || exit 1
 fi
 
 if [[ "$2" == "-a" ]]; then
@@ -77,6 +92,13 @@ if [[ "$1" == "-g" ]]; then
     build $1 $2
 fi
 
+if [[ "$1" == "-k" ]]; then
+    sed -i 's;gui="0";gui="1";' airootfs/root/customize_airootfs.sh
+    sed -i -r 's;version=".*";version="'$version'";' airootfs/root/customize_airootfs.sh
+    cp packages.x86_64_kde packages.x86_64
+    build $1 $2
+fi
+
 if [[ "$1" == "-s" ]]; then
     sed -i 's;gui="1";gui="0";' airootfs/root/customize_airootfs.sh
     sed -i -r 's;version=".*";version="'$version'";' airootfs/root/customize_airootfs.sh
@@ -89,6 +111,7 @@ if [[ "$1" == "-h" ]]; then
         echo "-h | help message"
         echo "-s | compile iso in server mode"
         echo "-g | compile iso in gui mode "
+        echo "-k | compile iso in kde mode "
         echo "-a | compile iso with azerty as keyboard layout (works with both -s and -g) This option must be the last option specified "
         exit 0
 fi
