fed36e30f75c7e5aa28c471222916a14d48358e5
F0xedb <tom@odex.be>
2019-10-18 12:23:05 +0200

    Added License marks at the top of each executable

--- toslive/start.sh
+++ toslive/start.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# MIT License
+# 
+# Copyright (c) 2019 Meyers Tom
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # This is a simple script to build an iso image. It prepares the build directory and then starts the build
 if [[ ! -f version.txt ]]; then
   ./version_builder.sh
@@ -136,3 +158,4 @@ if [[ "$1" == "-h" ]]; then
   echo "-a | compile iso with azerty as keyboard layout (works with both -s and -g) This option must be the last option specified "
   exit 0
 fi
+
