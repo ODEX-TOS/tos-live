6d85c4d851950da7f873a3bddbc21d9620cf11df
F0xedb <tom@odex.be>
2020-03-01 16:09:14 +0200

    Added progress when uploading data

--- repo/upload.sh
+++ repo/upload.sh
@@ -26,5 +26,5 @@
 if [[ "$1" != "-y" ]]; then
   read -p "Only upload if this is a full repo otherwise the repo database is wrong"
 fi
-rsync -rl arch root@pbfp.xyz:/root/repo/
+rsync -rl --progress arch root@pbfp.xyz:/root/repo/
 
