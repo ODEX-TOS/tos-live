4f848982600a59f638e4eef11fba8b329c978e6e
Tom <tom@odex.be>
2021-05-26 12:06:34 +0200

    Only show gpg key if it is valid

--- repo/gpg_expiry.sh
+++ repo/gpg_expiry.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Get the gpg expiry date
-gpg --list-key "$GPG_REPO_KEY" | head -n1 | grep -Eo "expires: ([0-9]+-[0-9]+-[0-9]+)" > arch/gpg-date.txt
+res=$(gpg --list-key "$GPG_REPO_KEY" | head -n1 | grep -Eo "expires: ([0-9]+-[0-9]+-[0-9]+)")
+
+if [[ -n "$res" ]]; then
+	echo $res" > "arch/gpg-date.txt
+fi
