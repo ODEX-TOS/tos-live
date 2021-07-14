bcbe2b79e13f06c26048a14708310394b7505c0d
Tom <tom@odex.be>
2021-07-14 16:39:16 +0200

    MISC: Add functionality to create the iso and then run it

--- toslive/run_iso.sh
+++ toslive/run_iso.sh
@@ -5,11 +5,13 @@ if [[ "$1" == "-h" ]]; then
   echo "-s | run server iso in qemu"
   echo "-t | run TDE iso in qemu"
   echo "-u | run it in UEFI mode"
+  echo "-c | create the iso from scratch"
   exit 0
 fi
 
 TDE="1"
 SERVER="0"
+CREATE="0"
 OPTS=""
 
 # TODO: verify that QEMU is installed, and when UEFI is enabled that edk2-ovmf is installed
@@ -30,6 +32,10 @@ while [[ "$1" != "" ]]; do
             OPTS="$OPTS -u"
             shift
         ;;
+        "-c")
+            CREATE="1"
+            shift
+        ;;
         **)
             echo "Unknown option $1"
             shift
@@ -39,8 +45,10 @@ done
 
 if [[ "$TDE" == "1" ]]; then
     ISO="out/toslive-awesome.iso"
+    [[ "$CREATE" == "1" ]] && sudo ./start.sh -awesome || exit 1
 elif [[ "$SERVER" == "1" ]]; then
     ISO="out/tosserver.iso"
+    [[ "$CREATE" == "1" ]] && sudo ./start.sh -s || exit 1
 fi
 
 run_archiso $OPTS -i "$ISO"
