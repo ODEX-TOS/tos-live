543007afd118b892b807f3a454d5e9df3756836a
F0xedb <tom@odex.be>
2023-03-01 16:47:58 +0100

    Update the the newer gpg key

--- repo/gpg_expiry.sh
+++ repo/gpg_expiry.sh
@@ -4,5 +4,7 @@
 res=$(gpg --list-key "$GPG_REPO_KEY" | head -n1 | grep -Eo "expires: ([0-9]+-[0-9]+-[0-9]+)")
 
 if [[ -n "$res" ]]; then
-	echo $res" > "arch/gpg-date.txt
+	echo "$res" > "arch/gpg-date.txt"
 fi
+
+gpg --export --armor "$GPG_REPO_KEY" > "arch/public.gpg"
